61a103e8d146d7b2536143e73f450a926a4dcef7
storm_broker <jiteshbalyan@gmail.com>
2026-09-12 16:15:26 +0530

    Create time_conversion.py

---
+++ time_conversion.py
@@ -0,0 +1,9 @@
+#convert hour into minute
+t = int(input("Enter hours :"))
+h = t *60
+print("In minutes :",h)
+
+#convert minutes into hour
+m = int(input("Enter how many minutes :"))
+mi = m /60
+print("In hours : ",mi)
