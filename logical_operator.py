26021b0ce30da526bdc928156961eac6c1849290
storm_broker <jiteshbalyan@gmail.com>
2026-09-12 16:51:33 +0530

    Create logical_operator.py

---
+++ logical_operator.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("a > 0 and b > 0:", a > 0 and b > 0)
+print("a > 0 or b > 0:", a > 0 or b > 0)
+print("not(a > 0):", not(a > 0))
